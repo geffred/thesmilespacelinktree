0beb93d634049e088385ed231bffd11f9460992e
geffred <tchapda20@gmail.com>
2026-01-15 12:51:41 +0100

    Update

--- src/LinktreePage.jsx
+++ src/LinktreePage.jsx
@@ -27,7 +27,7 @@ const LinktreePage = () => {
 
         <div className="links-section">
           <a
-            href="https://www.google.com/search?client=safari&sca_esv=29db07fc8cf0577a&hl=fr-be&q=avis+sur+the+smile+space&uds=AOm0WdE2fekQnsyfYEw8JPYozOKzkAn00025F2C_QwhT9HnqZyIhPyQgJYcNpDkhgyF0khJR9jU6LmClVlL3oW9UmlIOZQTLdNOfH4WXVIyrMt7G0icUFRsQCOXZCK_IVMiz4x26oc7x_Q5MJYSKCU-kUPyddx-7Zo4Dulszvcnm7KPJStnHhAE&si=AMgyJEtREmoPL4P1I5IDCfuA8gybfVI2d5Uj7QMwYCZHKDZ-E-JwdQ_Ty5x0FaxlrmQ66N_IC5hpw1BVUP_yKfmMiY-Wy7pCvIR1ltIGiRcjRTZY1gyDHNMtfa2zKb3YrKd27G92a5Um-1a4mH-TLahdzU1QGQuK8g%3D%3D&sa=X&ved=2ahUKEwi3hr37xaCOAxXuaqQEHZ8xHhYQxKsJKAB6BAgKEAE&ictx=1&stq=1&cs=1&lei=RGRmaLfUAu7VkdUPn-P4sAE"
+            href="https://share.google/Pq9I8di4dEohIa0lq"
             target="_blank"
             rel="noopener noreferrer"
             className="link-button"
@@ -35,7 +35,7 @@ const LinktreePage = () => {
             <div className="link-content">
               <span className="link-icon">
                 <img
-                  src="https://www.svgrepo.com/show/475656/google-color.svg"
+                  src="/google-color-svgrepo-com.svg"
                   alt="Google Icon"
                   className="google-icon"
                   style={{ width: "50px", height: "50px" }}
@@ -54,32 +54,13 @@ const LinktreePage = () => {
             <div className="link-content">
               <span className="link-icon">
                 <img
-                  src="https://www.svgrepo.com/show/452229/instagram-1.svg"
+                  src="/instagram-2-1-logo-svgrepo-com.svg"
                   alt="Instagram Icon"
                   className="instagram-icon"
                   style={{ width: "50px", height: "50px" }}
                 />
               </span>
               <span className="link-text">Suivez-nous sur Instagram</span>
-            </div>
-          </a>
-
-          <a
-            href="https://calendly.com/mons-thesmilespace/the-smile-space-nouveau-cabinet-d-orthodontie-a-mons"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="link-button primary-link"
-          >
-            <div className="link-content">
-              <span className="link-icon">
-                <img
-                  src="https://www.svgrepo.com/show/423027/calendar-date-schedule.svg"
-                  alt="Calendly Icon"
-                  className="calendly-icon"
-                  style={{ width: "50px", height: "50px" }}
-                />
-              </span>
-              <span className="link-text">Prendre rendez-vous !</span>
             </div>
           </a>
         </div>
